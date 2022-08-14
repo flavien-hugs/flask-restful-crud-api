0688427b83cf5b91f568fc2a640b40cc312d68cb
flavien-hugs <geekactux@gmail.com>
2022-08-14 00:53:07 +0000

    add decorator for return json

--- apps/task/routes.py
+++ apps/task/routes.py
@@ -2,49 +2,55 @@ from flask import Flask, request
 
 from .. import db, api
 from .models import Task
-from .serializers import taskFieldsSerializers
-from flask_restful import Resource, marshal_with
+from .serializers import task_fields
+from flask_restful import Resource, abort, marshal_with
 
 
-class Items(Resource):
-    @marshal_with(taskFieldsSerializers)
+class TaskList(Resource):
+    """
+    shows a list of all tasks, and lets you POST to add new tasks
+    """
+    @marshal_with(task_fields, envelope='resource')
     def get(self):
         tasks = Task.query.order_by(Task.timestamp.desc()).all()
-        return tasks
+        return tasks, 201
 
-    @marshal_with(taskFieldsSerializers)
+    @marshal_with(task_fields, envelope='resource')
     def post(self):
         data = request.json
         task = Task(name=data['name'], description=data['description'])
         db.session.add(task)
         db.session.commit()
         tasks = Task.query.order_by(Task.timestamp.desc()).all()
-        return tasks
+        return tasks, 201
 
 
-class Item(Resource):
-    @marshal_with(taskFieldsSerializers)
+class TaskSingle(Resource):
+    """
+    shows a single todo item and lets you delete a todo item
+    """
+    @marshal_with(task_fields, envelope='resource')
     def get(self, pk):
         task = Task.query.filter_by(id=pk).first()
-        return task
+        return task, 201
 
-    @marshal_with(taskFieldsSerializers)
+    @marshal_with(task_fields, envelope='resource')
     def put(self, pk):
         data = request.json
         task = Task.query.filter_by(id=pk).first()
         task.name = data['name']
         task.description = data['description']
         db.session.commit()
-        return task
+        return task, 201
 
-    @marshal_with(taskFieldsSerializers)
+    @marshal_with(task_fields, envelope='resource')
     def delete(self, pk):
         task = Task.query.filter_by(id=pk).first()
         db.session.delete(task)
         db.session.commit()
         tasks = Task.query.order_by(Task.timestamp.desc()).all()
-        return tasks
+        return tasks, 204
 
 
-api.add_resource(Items, '/')
-api.add_resource(Item, '/<int:pk>')
+api.add_resource(TaskList, '/')
+api.add_resource(TaskSingle, '/<int:pk>/')
